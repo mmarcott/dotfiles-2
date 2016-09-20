5974fb497ca11ee8722c876869a632d64e60a9eb
Rainbow <rainbowtupperware@openmailbox.org>
2016-09-20 04:29:12 -0300

    bar geometry edited and avoid close it with left click

--- bar.sh
+++ bar.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+#herbst
+#hc() { "${herbstclient_command[@]:-herbstclient}" "$@" ;}
+#hc pad 0 32
+
 #ICONS
 CLOCK_ICON="/home/rainbow/icons/clock.xbm"
 MEM_ICON="/home/rainbow/icons/mem.xbm"
 TEMP_ICON="/home/rainbow/icons/weather.xbm"
 
 #PRINTF
+
 
 while :; do
 
@@ -14,5 +19,5 @@ while :; do
    echo "^fg(#FFBBBB)^i($CLOCK_ICON) $TIME^fg() ^fg(#DBBFF7)^i($MEM_ICON) $MUSED^fg()  ^fg(#E1E1FF)^i($TEMP_ICON) $(~/scripts/owm.py)^fg()"
    sleep 2
 
-done | dzen2 -fn "lemon" -bg "#273941" -fg "#FFFFFF" -geometry "600x25+400+5"
+done | dzen2 -fn "lemon" -bg "#273941" -fg "#FFFFFF" -e 'button2=;' #-geometry "600x25+400+5"
 
